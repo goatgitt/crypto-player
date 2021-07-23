a1607213f35677c5d535bee88aa26915758ce0a0
goatgitt <75272099+goatgitt@users.noreply.github.com>
2021-07-23 20:57:14 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', "", ".webpack.js", ".web.js", ".ts"],
   },
   output: {
     filename: 'script.js',
