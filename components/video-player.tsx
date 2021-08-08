9efec23003269d3f2c9d40e8fad5bb2a9d0609a6
goatgitt <75272099+goatgitt@users.noreply.github.com>
2021-08-08 21:54:06 +0200

    Update video-player.tsx

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -34,6 +34,22 @@ export default function VideoPlayer({ src, poster }) {
   const onVideo = useCallback((el) => {
     setVideoEl(el);
   }, []);
+async function getWalletAccessToken() {
+    //Check if a user is logged in...
+    let isAuthenticated = true;
+
+    if (!isAuthenticated) {
+        //No user is logged in, no wallet will be used
+        return null;
+    }
+
+    //This API should check the user's auth 
+    let body = await yourAPIRequestToGenerateThetaWalletAccessTokenForAuthedUser();
+
+    //Return the access token from the request body
+    return body.access_token;
+}
+
 
   useEffect(() => {
     if (videoEl == null) return;
