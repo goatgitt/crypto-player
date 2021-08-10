4e48211b492f2d036a85449c9de30e942a29cee4
goatgitt <75272099+goatgitt@users.noreply.github.com>
2021-08-10 21:29:05 +0200

    Update expose-components.js

--- components/expose-components.js
+++ components/expose-components.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactDOMServer from 'react-dom/server';
 
-import RootComponent from './home.jsx';
-
 // any css-in-js or other libraries you want to use server-side
 import { ServerStyleSheet } from 'styled-components';
 import { renderStylesToString } from 'emotion-server';
@@ -16,4 +14,3 @@ global.ReactDOMServer = ReactDOMServer;
 global.Styled = { ServerStyleSheet };
 global.Helmet = Helmet;
 
-global.Components = { RootComponent };
