d6ce6ae44984babc08e730e668e6fccbf026d64a
goatgitt <75272099+goatgitt@users.noreply.github.com>
2021-08-11 18:10:17 +0200

    Update index.d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,15 +3,15 @@ declare module "react"
 declare module "react-icons/fi"
 declare module "@sindresorhus/slugify"
 export const jsx: typeof React.createElement
-*/
-interface SxProps {
-  sx: SystemStyleObject
-}
 
-type SxComponent = React.ComponentClass<SxProps>
-  export const Box: SxComponent
-  export const Container: SxComponent
-  export const Flex: SxComponent
-  export const Header: SxComponent
-  export const Styled: { [k: string]: SxComponent }
-*/
+//interface SxProps {
+//  sx: SystemStyleObject
+//}
+
+//type SxComponent = React.ComponentClass<SxProps>
+//  export const Box: SxComponent
+//  export const Container: SxComponent
+//  export const Flex: SxComponent
+//  export const Header: SxComponent
+//  export const Styled: { [k: string]: SxComponent }
+
