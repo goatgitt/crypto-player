5286d943c5f8bd2453235e353dfeddb2189694d0
goatgitt <75272099+goatgitt@users.noreply.github.com>
2021-08-11 12:17:01 +0200

    Update index.d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,10 @@ declare module "theme-ui"
 declare module "react"
 declare module "react-icons/fi"
 declare module "@sindresorhus/slugify"
+export const jsx: typeof React.createElement
+
+interface SxProps {
+  sx: SystemStyleObject
+}
+
+type SxComponent = React.ComponentClass<SxProps>
